553117a91721752df690c5af41a3a89566f5df88
Le Dam Duy Phuc <somdp2907@gmail.com>
2025-01-14 15:54:45 +0700

    feat(nvim): update nvim config

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -1,7 +1,7 @@
 
 # fnm
-set FNM_PATH "/home/focal/.local/share/fnm"
+set FNM_PATH "$HOME/.local/share/fnm"
 if [ -d "$FNM_PATH" ]
-  set PATH "$FNM_PATH" $PATH
-  fnm env | source
+    set PATH "$FNM_PATH" $PATH
+    fnm env | source
 end
---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,44 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+local nomap = vim.keymap.del
+-- remove key from nvchad
+nomap("n", "<leader>wK")
+nomap("n", "<leader>wk")
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+-- save on leader w
+map("n", "<leader>w", "<cmd>w<CR>", { desc = "general save file", nowait = true })
+-- Save with C-s in insert mode
+vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
+-- Select All
+vim.api.nvim_set_keymap("n", "<C-a>", "ggVG", { silent = true })
+
+map("n", "<leader>x", "<cmd>:lua require('nvchad.tabufline').close_buffer()<CR>")
+map("n", "<leader>lg", "<cmd>:lua require('logsitter').log()<CR>")
+
+-- Set up terminal mappings
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    local opts = { noremap = true, silent = true, buffer = true }
+    -- Map <C-k> to move to the above buffer
+    vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
+    -- Map <C-j> to move to the below buffer
+    vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
+    -- Additional terminal navigation mappings (optional)
+    vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], opts)
+    vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
+  end,
+})
+-- Setup for leap
+vim.keymap.set({ "n", "x", "o" }, "ga", function()
+  require("leap.treesitter").select()
+end)
+
+-- Linewise.
+vim.keymap.set({ "n", "x", "o" }, "gA", 'V<cmd>lua require("leap.treesitter").select()<cr>')
+
+-- toggle nvimtree
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree", nowait = true })
