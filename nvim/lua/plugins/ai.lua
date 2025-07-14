846736a0265b1231a42777a0fdd81ce72103f9f5
Le Dam Duy Phuc <permees@nexondv.com>
2025-07-14 15:21:25 +0700

    feat(nvim, hypr): update config

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -53,27 +53,7 @@ return {
             adapter = "copilot",
           },
         },
-        extensions = {
-          vectorcode = {
-            opts = {
-              add_tool = true,
-              add_slash_command = true,
-              tool_opts = {
-                -- use_lsp = true,
-              },
-            },
-          },
-        },
       })
     end,
-  },
-  {
-    "Davidyz/VectorCode",
-    version = "*", -- optional, depending on whether you're on nightly or release
-    build = "pipx upgrade vectorcode", -- optional but recommended. This keeps your CLI up-to-date.
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      -- async_backend = "lsp",
-    },
   },
 }
