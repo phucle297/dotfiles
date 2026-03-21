f9abfa77dee417c052d88f80d1d011692035f7aa
PerMees <somdp2907@gmail.com>
2026-03-21 11:33:53 +0000

    feat(tabby): update conf

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -39,3 +39,24 @@ curl -fsSL https://fnm.vercel.app/install | bash
 
 # Install dotfiles
 yes | ghq get git@github.com:phucle297/dotfiles.git
+
+# Install docker
+bash
+sudo apt update -y
+sudo apt install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt update -y
+apt-cache policy docker-ce
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo usermod -aG docker ${USER}
+su - ${USER}
+
+# Install ClaudeCode
+curl -fsSL https://claude.ai/install.sh | bash
